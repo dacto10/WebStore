f407e566ce215fd05c917371d935ffa929a7fa75
dario.ruiz <dario.ruiz@alumnos.uneatlantico.es>
2021-12-03 14:34:51 +0100

    feat: query param url para el input busqueda

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,37 +1,48 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom'
 import logo from '../assets/icons/logo.svg'
+import { useLocation } from 'react-router-dom';
 
-const Navbar: React.FC = () => (
-    <div className="navbar">
-        <Link to={'/'} className="navbar__logo">
+const Navbar: React.FC = () => {
+    const navigate = useNavigate();
+    const location = useLocation();
+    const [search, setSearch] = useState<string>(new URLSearchParams(location.search).get("s") ?? "");
+    
+    const handleSearch = () => {
+        search.trim() !== "" && navigate(`/?s=${search}`);
+    }
+
+    return (
+        <div className="navbar">
+            <Link to={'/'} className="navbar__logo">
                 <img src={ logo } alt="" />
                 <span>WebStore</span>
-        </Link>
-        <div className="navbar__search search">
-            <input type="text" placeholder="Search for a product..." />
-            <button type="button">
-                <FontAwesomeIcon icon={['fas', 'search']}/>
-            </button>
-        </div>
-        <Link to={'/cart'} className="navbar__cart">
-            <FontAwesomeIcon icon={['fas', 'shopping-cart']}/>
-            <span>My Cart</span>
-        </Link>
-        <div className="navbar__user">
-            <FontAwesomeIcon icon={['fas', 'user']}/>
-            <span>username</span>
-        </div>
-        <div className="navbar__dropdown">
-            <Link to={'/cart'}>
-                <span>My Products</span>
             </Link>
-            <Link to={'/login'}>
-                <span>Logout</span>
+            <div className="navbar__search search">
+                <input type="text" placeholder="Search for a product..." value={search} onChange={e => setSearch(e.target.value)}/>
+                <button type="button" onClick={handleSearch}>
+                    <FontAwesomeIcon icon={['fas', 'search']}/>
+                </button>
+            </div>
+            <Link to={'/cart'} className="navbar__cart">
+                <FontAwesomeIcon icon={['fas', 'shopping-cart']}/>
+                <span>My Cart</span>
             </Link>
+            <div className="navbar__user">
+                <FontAwesomeIcon icon={['fas', 'user']}/>
+                <span>username</span>
+            </div>
+            <div className="navbar__dropdown">
+                <Link to={'/cart'}>
+                    <span>My Products</span>
+                </Link>
+                <Link to={'/login'}>
+                    <span>Logout</span>
+                </Link>
+            </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default Navbar;
